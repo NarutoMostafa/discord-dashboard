6cddbb42ea92fa96669819d7db727cfa1a62884f
Naruto <168789660+NarutoMostafa@users.noreply.github.com>
2024-09-01 16:29:39 +0300

    Update example.config.js

--- example.config.js
+++ example.config.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-inline-comments */
 
 module.exports = {
-  port: 8000, // Pport to listen on
+  port: 3000, // Pport to listen on
   prefix: "!", // Prefix for commands
-  id: "ID", // Discord Bot ID
+  id: "1268264734596010145", // Discord Bot ID
   usingCustomDomain: false, // Use a custom domain for the bot
-  domain: "http://localhost", // Custom domain
-  discordInvite: "https://discord.gg/", // Discord Invite URL
-  mongodbUrl: "MONGO_URI", // MongoDB connection URL
-  clientSecret: "CLIENT_SECRET", // Client Secret from Discord Application
-  token: "CLIENT_TOKEN", // Bot Token from Discord Application
+  domain: "http://localhost:3000", // Custom domain
+  discordInvite: "https://discord.com/oauth2/authorize?client_id=1268264734596010145&permissions=8&integration_type=0&scope=bot+applications.commands", // Discord Invite URL
+  mongodbUrl: "mongodb+srv://c4clan1:c4clan1@c4clan.ohxsoxt.mongodb.net/?retryWrites=true&w=majority", // MongoDB connection URL
+  clientSecret: "wYf7DQGevYT_QPTssMhPsLfKjX23dXLX", // Client Secret from Discord Application
+  token: "MTI3Mjc3NTk0NzMzODM4MzM5MA.G4GlSy.OibFDJ2j3UFXWTIeSP4rDI9fet8AJ6mfCBxzxs", // Bot Token from Discord Application
 };
 
 /**
